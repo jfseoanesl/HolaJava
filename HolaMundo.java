dc93366c1e084535bf777fe0670a0527ed08fc42
jfseoanesl <35581581+jfseoanesl@users.noreply.github.com>
2020-09-06 12:00:16 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+/* Elaborado por:  Jairo F. Seoanes Leon
+   Fecha:          06-09-2020
+   Proposito:      Imprime en pantalla un mensaje de bienvenida
+*/		 	
+
+
+// definicion de la clase principal
+public class HolaMundo {
+
+	//definicion del metodo principal
+	public static void main(String arg[]){
+
+		// instruccion de salida de datos por consola
+		System.out.println("Hola Java");
+	}
+
+}
